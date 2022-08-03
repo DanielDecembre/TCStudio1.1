2b300ec9de954ba6865432f5fa8c9fc174ef9376
daniel decembre <danieldecembre@daniels-iMac.local>
2022-08-03 14:58:22 -0400

    wip; set up model and routes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 // DEPENDENCIES
 const express = require('express');
 const mongoose = require('mongoose'); 
-
+const morgan = require('morgan')
+const cors = require('cors');
 
 // Initialize the Express App
 const app = express();
@@ -22,13 +23,59 @@ mongoose.connection
 .on('error', (err) => console.log('Error with MongoDB: ' +err.message))
 
 
+// Set up our Model
+const memberSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String,
+}, {timestamps: true});
+
+const Member = mongoose.model('Member', memberSchema);
+
 // Mount Middleware
- 
+ app.use(cors());  // Access-Control-Allow: '*'
+ app.use(morgan('dev'));
+ app.use(express.json()); 
+ // this creates req.body from incoming JSON request bodies
+ //app.use(express.urlencoded({ extended: false }))
+ // only when express is serving HTML
+
+
+
 
 // Mount Routes
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+// Index
+app.get('/member', async (req, res) => {
+    try {
+        const member = await Member.find({});
+        res.send(member);
+    } catch (error) {
+        console.log('error: ', error);
+        res.send({error: 'something went wrong - check console'});
+    }
+});
+
+// Create 
+app.post('/member', async (req, res) => {
+    try {
+        const customer = await Member.create(req.body);
+        res.send(customer);
+    } catch (error) {
+        console.log('error: ', error);
+        res.send({error: 'something went wrong - check console'});
+    }
+});
+
+// Update 
+
+
+// Delete
+
+
 
 
 // Tell Express to Listen
